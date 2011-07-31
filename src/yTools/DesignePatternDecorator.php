f6ea732730112a50577126500a03a0cdeb844007
Janusz Jablonski <januszjablonski.pl@gmail.com>
2011-07-31 02:26:05 +0200

    Add license.

--- src/yTools/DesignePatternDecorator.php
+++ src/yTools/DesignePatternDecorator.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the yTools package.
+ *
+ * (c) Janusz Jablonski <januszjablonski.pl@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace yTools;
 
 class DesignePatternDecorator {
