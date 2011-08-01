1f51934dd751a37c5e77cb886c6d6c97765e736d
Janusz Jablonski <januszjablonski.pl@gmail.com>
2011-08-01 03:46:32 +0200

    Create autoloader.

---
+++ autoloader.php
@@ -0,0 +1,14 @@
+<?php
+$src = realpath(__DIR__ . '/src');
+spl_autoload_register(function ($name) use ($src) {
+    exit($name);
+    $path = $src . '/' .
+        str_replace(array('\\'), array(DIRECTORY_SEPARATOR), $name) .
+        '.php';
+    if (file_exists($path)) {
+        require_once($path);
+        return true;
+    } else {
+        return false;
+    }
+});
